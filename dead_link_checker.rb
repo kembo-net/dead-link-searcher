881938bf884c207e1480f4b148a3c2c2def92a63
kembo <lupin@kembo.org>
2017-05-29 00:05:02 +0900

    検査結果をまとめて表示するように修正 ついでにいくつかのバグを回収

--- dead_link_checker.rb
+++ dead_link_checker.rb
@@ -5,11 +5,15 @@ $URL_PATTERN = '(?!mailto:)(?:https?:\/\/)?[\w\/:%#\$&\?\(\)~\.=\+\-]+'
 
 @root_url_str = ARGV[0]
 @root_uri = URI.parse(@root_url_str)
-@root_http = Net::HTTP.new(@root_uri.host, @root_uri.port)
-@root_http.use_ssl = @root_uri.kind_of?(URI::HTTPS)
+@root_http=Net::HTTP.new(@root_uri.host, @root_uri.port)
+if @root_uri.kind_of?(URI::HTTPS)
+  @root_http.use_ssl = true
+  @root_http.verify_mode = OpenSSL::SSL::VERIFY_NONE # なんかSSL証明書エラーが出るんで検証しない
+end
 @yomikae_host = ARGV[1]
 
 @stack = [[@root_url_str, true, '/', -1]]
+@results = []
 @log_loc = []
 @log_glb = []
 
@@ -31,7 +35,10 @@ def analyze_url(url_str)
   rescue Exception => e
     raise e
   end
-  return [http, path || '/', is_local]
+  if path.nil? || path.empty?
+    path = '/'
+  end
+  return [http, path, is_local]
 end
 
 # 訪問済みかどうかのチェック
@@ -55,33 +62,50 @@ end
 # ページの存在を確認し、存在しない場合結果を表示するメソッド
 # ローカルの場合は再帰的探索も行う
 until @stack.empty?
-  url_str, recursion, from_path, line_num = @stack.pop
+  url_str, recursion, from_path, line_num = @stack.shift
+
+  # 進捗の表示
+  stat_message = from_path + ":#{line_num} " + url_str
+  if stat_message.length <= 100
+    stat_message = stat_message + (' ' * (100 - stat_message.length))
+  else
+    stat_message = stat_message[0..97] + '...'
+  end
+  print("\r" + stat_message)
+
   # URLを解析する 相対パスも考慮
   if url_str.match(/^http/)
     begin
       http, path, is_local = analyze_url(url_str)
     rescue Exception => e
-      puts "#{from_path}:#{line_num}(unknown error) #{url_str}"
-      return nil
+      @results.push({
+        path: from_path,
+        line: line_num,
+        message: 'unknown error',
+        url: url_str
+      })
+      next
     end
   else
     http = @root_http
     path = URI.join(@root_uri.to_s, from_path, url_str).path
     is_local = true
   end
-  if path.length == 0
-    path = '/'
-  end
 
-  # 探索済だったら帰る
-  return nil if check_log(is_local, is_local ? path : url_str)
+  # 探索済だったら飛ばす
+  next if check_log(is_local, is_local ? path : url_str)
 
   # アクセス出来るかどうか
   begin
     response = http.get(path)
   rescue Exception => e
-    puts "#{from_path}:#{line_num}(connection error) #{url_str}"
-    return nil
+    @results.push({
+      path: from_path,
+      line: line_num,
+      message: 'connection error',
+      url: url_str
+    })
+    next
   end
   location = nil
   cnt = 0
@@ -90,16 +114,25 @@ until @stack.empty?
     cnt += 1
     if cnt > 10
       # リダイレクトループしすぎだぞ
-      puts "#{from_path}:#{line_num}(too redirect) #{url_str}"
+      @results.push({
+        path: from_path,
+        line: line_num,
+        message: 'too redirect',
+        url: url_str
+      })
     end
     http, path, is_local = analyze_url(response['location'])
-    return nil if check_log(is_local, is_local ? path : url_str)
     response = http.get(path)
   end
   unless response.is_a?(Net::HTTPSuccess)
     # リンク切れだぞ
-    puts "#{from_path}:#{line_num}(#{response.code}) #{url_str}"
-    return response
+    @results.push({
+      path: from_path,
+      line: line_num,
+      message: response.code,
+      url: url_str
+    })
+    next
   end
   # 成功だぞ
   #puts "#{from_path}:#{line_num}(#{response.code}) #{url_str}"
@@ -119,3 +152,13 @@ until @stack.empty?
     end
   end
 end
+
+# 整形して結果の表示
+message = 'Completed!'
+print("\r" + message + (' ' * (100 - message.length)) + "\n")
+@results.sort { |a, b|
+  a[:path] == b[:path] ? a[:line] <=> b[:line]
+                       : a[:path] <=> b[:path] }
+@results.each { |r|
+  puts "#{r[:path]}:#{r[:line]}(#{r[:message]}) #{r[:url]}"
+}
